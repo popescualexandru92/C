2aac49e002dd8ae158f5223cf4fbca5576a89cac
AlexP92 <51406031+AlexP92@users.noreply.github.com>
2024-07-23 22:40:44 +0300

    Update

---
+++ test/1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main()
+{   
+    int n;
+    int a[] = {1, 2, 3, 4, 5};
+    FILE *f;
+    f = fopen("test.dat","wb");
+    if(!f)
+    printf("error");
+    else{
+        while(!feof(stdin)){
+            printf("Enter a numer : \n");
+            scanf("%d", &n);
+            fwrite(&n,sizeof(int),1,f);
+        }
+    }
+    fclose(f);
+}
---
+++ test/s.h
@@ -0,0 +1,9 @@
+#ifndef S_H_
+#define S_H_
+typedef struct {
+    char name[50];
+    int age;
+    
+} Person;
+
+#endif /* S_H_ */
